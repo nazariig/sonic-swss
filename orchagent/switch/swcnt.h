a88e611e329bd86117920d8ca293d59c7cac6712
Nazarii Hnydyn <nazariig@nvidia.com>
2023-01-16 15:58:20 +0200

    [hash]: GH backend rev02.
    Signed-off-by: Nazarii Hnydyn <nazariig@nvidia.com>

--- orchagent/switch/swcnt.h
+++ orchagent/switch/swcnt.h
@@ -8,16 +8,7 @@ extern "C" {
 #include <set>
 #include <string>
 
-class SwitchContainer
-{
-public:
-    SwitchContainer() = default;
-    virtual ~SwitchContainer() = default;
-
-    std::unordered_map<std::string, std::string> fieldValueMap;
-};
-
-class SwitchHash final : public SwitchContainer
+class SwitchHash final
 {
 public:
     SwitchHash() = default;
@@ -32,4 +23,6 @@ public:
         std::set<sai_native_hash_field_t> value;
         bool is_set = false;
     } lag_hash;
+
+    std::unordered_map<std::string, std::string> fieldValueMap;
 };
